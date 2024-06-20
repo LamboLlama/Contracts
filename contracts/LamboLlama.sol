793d5c5996ff4612d3bf001defcf91dbe6e0a16a
Krasotulia <yozhyn1@gmail.com>
2024-06-20 18:23:55 +0100

    fix

--- contracts/LamboLlama.sol
+++ contracts/LamboLlama.sol
@@ -4,8 +4,6 @@ pragma solidity 0.8.22;
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {OFT} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/OFT.sol";
 
-import "hardhat/console.sol";
-
 contract LamboLlama is ERC20Permit, OFT {
     uint256 constant INITIAL_CHAIN_ID = 100;
 
@@ -15,8 +13,6 @@ contract LamboLlama is ERC20Permit, OFT {
         address _lzEndpointAddress
     ) ERC20Permit("LamboLlama") OFT("LamboLlama", "LLL", _lzEndpointAddress, _delegate) {
         _transferOwnership(_delegate);
-
-        console.log(block.chainid);
 
         if (block.chainid == INITIAL_CHAIN_ID) {
             _mint(_delegate, _supply);
